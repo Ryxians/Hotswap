13c38e8e264b2fe6fda797fc3f10b715606d1d7d
Ryxian <Ryxians@gmail.com>
2021-03-03 16:49:05 -0600

    Implement block and world checking, change trigger and check for nulls.

--- src/main/java/homebrew/hotswap/Hotswap.java
+++ src/main/java/homebrew/hotswap/Hotswap.java
@@ -1,7 +1,12 @@
 package homebrew.hotswap;
 
+import org.bukkit.World;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
 
 public final class Hotswap extends JavaPlugin {
 
@@ -13,16 +18,46 @@ public final class Hotswap extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        //set instance
+        // set instance
         instance = this;
+
+        // Variables
+        List<String> worlds;
 
         //load config
         config.options().copyDefaults(true);
         saveDefaultConfig();
 
-        // Plugin startup logic
-        getServer().getPluginManager().registerEvents(new PlayerInteract(), this);
+        // Code to run on initial installation
+        if (config.getBoolean("first")) {
+            // Get the server's worlds
+            worlds = new LinkedList<String>();
+            for (World world : getServer().getWorlds()) {
+                // Add each world name
+                worlds.add(world.getName());
+                getLogger().info("Adding: " + world.getName());
+            }
+            config.createSection("worlds.default");
+            config.set("worlds.default", worlds);
+            config.set("first", false);
+            saveConfig();
 
+        } else {
+            worlds = (List<String>) config.getList("worlds.default");
+        }
+        if (!config.getBoolean("worlds.all")) {
+            for (String world : worlds) {
+                World temp = getServer().getWorld(world);
+                if (temp != null) {
+                    getServer().getPluginManager().registerEvents(new PlayerInteract(temp), this);
+                    getLogger().info("Adding: " + world);
+                }
+            }
+        } else {
+            getServer().getPluginManager().registerEvents(new PlayerInteract(), this);
+        }
+
+        getLogger().info("Hotswap: Enabled");
     }
 
     @Override
--- src/main/java/homebrew/hotswap/PlayerInteract.java
+++ src/main/java/homebrew/hotswap/PlayerInteract.java
@@ -1,9 +1,12 @@
 package homebrew.hotswap;
 
 import org.bukkit.Sound;
+import org.bukkit.World;
+import org.bukkit.block.Container;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
@@ -20,72 +23,95 @@ public class PlayerInteract implements Listener {
     boolean leggings = config.getBoolean("swap.leggings");
     boolean boots = config.getBoolean("swap.boots");
     boolean elytra = config.getBoolean("swap.elytra");
+    boolean container = config.getBoolean("container-swap");
+    World world = null;
     boolean swapped;
     Cancellable event;
 
+    // Constructors
+    public PlayerInteract() {
+
+    }
+
+    public PlayerInteract(World world) {
+        this.world = world;
+    }
+
+
     @EventHandler
+    public void playerInteractCondition(PlayerInteractEvent evt) {
+        if (world == null || evt.getPlayer().getWorld().equals(world)) {
+            playerInteract(evt);
+        }
+    }
+
     public void playerInteract(PlayerInteractEvent evt) {
         event = evt;
         Player player = evt.getPlayer();
-        //When a player right clicks it right clicks for both hands, we only want the main hand
-        switch (evt.getHand()) {
-            //For the Main Hand swap everything.
-            case HAND:
-                //For Right Click we want to perform the swap, otherwise we don't want this plugin to do anything.
-                switch (evt.getAction()) {
-                    case RIGHT_CLICK_AIR:
-                    case RIGHT_CLICK_BLOCK:
-                        //Convert the item in hand to string array
-                        String strv[] = player.getInventory().getItemInMainHand().getType().toString().split("_");
+        boolean doSwap = true;
 
-                        //Get the Player's Inventory
-                        PlayerInventory pi = player.getInventory();
-                        swapped = false;
+        // Check if they used a container
+        if (evt.getClickedBlock() != null && container)
+            if (evt.getClickedBlock().getState() instanceof Container)
+                doSwap = false;
 
-                        //Do the correct swap based on what is being held
-                        switch (strv[strv.length-1]){
-                            case "ELYTRA":
-                                if (!player.hasPermission("hotswap.elytra")) {
-                                    break;
-                                }
-                                if (!elytra) {
-                                    break;
-                                }
-                            case "CHESTPLATE":
-                                if (chestplate && player.hasPermission("hotswap.chest")) {
-                                    pi.setChestplate(swap(pi, pi.getItemInMainHand(), pi.getChestplate()));
-                                }
-                                break;
-                            case "HELMET":
-                                if (helmet && player.hasPermission("hotswap.helmet")) {
-                                    pi.setHelmet(swap(pi, pi.getItemInMainHand(), pi.getHelmet()));
-                                }
-                                break;
-                            case "LEGGINGS":
-                                if (leggings && player.hasPermission("hotswap.leggings")) {
-                                    pi.setLeggings(swap(pi, pi.getItemInMainHand(), pi.getLeggings()));
-                                }
-                                break;
-                            case "BOOTS":
-                                if (boots && player.hasPermission("hotswap.boots")) {
-                                    pi.setBoots(swap(pi, pi.getItemInMainHand(), pi.getBoots()));
-                                }
-                                break;
-                            default:
-                                break;
-                        }
-                        if (swapped) {
-                            player.playSound(player.getLocation(), Sound.ITEM_ARMOR_EQUIP_ELYTRA, 2, 0);
-                        }
-                        break;
-                    //For non right clicks
-                    default:
-                        break;
+        // On right click, swap
+        if (evt.useItemInHand().equals(Event.Result.DEFAULT) && doSwap) {
+            switch (evt.getAction()) {
+                case RIGHT_CLICK_AIR:
+                case RIGHT_CLICK_BLOCK:
+                    toSwap(player);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+    }
+
+    // Gets the item in the player's hand and swaps it to the correct location
+    public void toSwap(Player player) {
+        //Convert the item in hand to string array
+        String[] strv = player.getInventory().getItemInMainHand().getType().toString().split("_");
+
+        //Get the Player's Inventory
+        PlayerInventory pi = player.getInventory();
+        swapped = false;
+
+        //Do the correct swap based on what is being held
+        switch (strv[strv.length - 1]) {
+            case "ELYTRA":
+                if (!player.hasPermission("hotswap.elytra")) {
+                    break;
+                }
+                if (!elytra) {
+                    break;
+                }
+            case "CHESTPLATE":
+                if (chestplate && player.hasPermission("hotswap.chest")) {
+                    pi.setChestplate(swap(pi, pi.getItemInMainHand(), pi.getChestplate()));
                 }
                 break;
-            //For any non-main hand interact do nothing
+            case "HELMET":
+                if (helmet && player.hasPermission("hotswap.helmet")) {
+                    pi.setHelmet(swap(pi, pi.getItemInMainHand(), pi.getHelmet()));
+                }
+                break;
+            case "LEGGINGS":
+                if (leggings && player.hasPermission("hotswap.leggings")) {
+                    pi.setLeggings(swap(pi, pi.getItemInMainHand(), pi.getLeggings()));
+                }
+                break;
+            case "BOOTS":
+                if (boots && player.hasPermission("hotswap.boots")) {
+                    pi.setBoots(swap(pi, pi.getItemInMainHand(), pi.getBoots()));
+                }
+                break;
             default:
                 break;
+        }
+        if (swapped) {
+            player.playSound(player.getLocation(), Sound.ITEM_ARMOR_EQUIP_ELYTRA, 2, 0);
         }
     }
 
